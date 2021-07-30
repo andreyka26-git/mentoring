3aa314709f2b3106f45aa3f578ead3ae3d276dd9
Serhii Yurko <Serhii.Yurko@lnu.edu.ua>
2021-07-30 21:26:00 +0300

    First task implementation

--- ExceptionHandling/Task1/Program.cs
+++ ExceptionHandling/Task1/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Task1
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task1
 {
     internal class Program
     {
+        private static string GetFirstLetter(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentException("Input line is empty", nameof(text));
+            }
+
+            return text[0].ToString();
+        }
+
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            const int lineCount = 5;
+            var firstLettersResult = new List<string>();
+            try
+            {
+                for (var i = 0; i < lineCount; i++)
+                {
+                    var text = Console.ReadLine();
+                    firstLettersResult.Add(GetFirstLetter(text));
+                    Console.ReadLine();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            firstLettersResult.ForEach(letter => Console.Write($"{letter} "));
         }
     }
 }
