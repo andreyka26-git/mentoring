7b1f6633cd23676ad782a127dde83f836c5bcfb5
Сергій Юрко <Serhii.Yurko@lnu.edu.ua>
2021-08-17 15:14:14 +0300

    Complete second task

--- DataStructures/Tasks/HybridFlowProcessor.cs
+++ DataStructures/Tasks/HybridFlowProcessor.cs
@@ -5,24 +5,33 @@ namespace Tasks
 {
     public class HybridFlowProcessor<T> : IHybridFlowProcessor<T>
     {
+        private readonly IDoublyLinkedList<T> _linkedList = new DoublyLinkedList<T>();
+
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if (_linkedList.Length == 0)
+                throw new InvalidOperationException();
+
+            return _linkedList.RemoveAt(0);
         }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            _linkedList.Add(item);
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (_linkedList.Length == 0)
+                throw new InvalidOperationException();
+
+            var lastIndex = _linkedList.Length - 1;
+            return _linkedList.RemoveAt(lastIndex);
         }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            _linkedList.Add(item);
         }
     }
 }
