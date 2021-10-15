e411ee8e5a42190dc9c7820817361841ac1b61bc
Сергій Юрко <Serhii.Yurko@lnu.edu.ua>
2021-10-15 10:58:34 +0300

    Make fields for aggregate private.

--- WebAPI/WebAPI.BusinessLogic/Aggregates/EmployeeAggregate/Employee.cs
+++ WebAPI/WebAPI.BusinessLogic/Aggregates/EmployeeAggregate/Employee.cs
@@ -3,9 +3,18 @@
     public class Employee
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public bool IsHigherEducation { get; set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public bool IsHigherEducation { get; private set; }
         public int? ProjectId { get; set; }
+        public Employee() { }
+        public Employee(int id, string firstName, string lastName, bool isHigherEducation, int projectId)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            IsHigherEducation = isHigherEducation;
+            ProjectId = projectId;
+        }
     }
 }
--- WebAPI/WebAPI.BusinessLogic/Aggregates/ProjectAggregate/Project.cs
+++ WebAPI/WebAPI.BusinessLogic/Aggregates/ProjectAggregate/Project.cs
@@ -3,7 +3,15 @@
     public class Project
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int Duration { get; set; }
+        public string Name { get; private set; }
+        public int Duration { get; private set; }
+        public Project() { }
+
+        public Project(int id, string name, int duration)
+        {
+            Id = id;
+            Name = name;
+            Duration = duration;
+        }
     }
 }
